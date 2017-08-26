2db1388a021bcfba1da717da8b244134094a25db
UnderADome <mushi2016@outlook.com>
2017-08-26 20:46:31 +0800

    Create 折半插入排序简单实现.c
    最简单的实现

---
+++ 折半插入排序简单实现.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(void)
+{
+	int b[13] = {45,32,48,65,12,43,62,59,68,32,15,45,55};
+	printf("原始数列:");
+	for (int i=0;i<13;i++)		
+		printf("%d ", b[i]);
+	printf("\n");
+	int i, j, mid, low, high, temp;
+	for (i=0;i<13;i++)
+	{
+		temp = b[i+1];
+		low = 0;
+		high = i;
+		while(low<=high)
+		{
+			mid = (low+high)/2;
+			if (b[mid]>temp)
+				high = mid-1;
+			else
+				low = mid +1;
+		}
+		for (j=i; j>=low; j--)
+		{
+			b[j+1] = b[j];
+		}
+		b[low] = temp;
+		printf("第%d次排序：", i+1);
+		for (int i=0;i<13;i++)
+			printf("%d ", b[i]);
+		printf("\n");
+	}
+	
+	for (int i=0;i<13;i++)
+		printf("%d ", b[i]);
+	return 0;
+} 
